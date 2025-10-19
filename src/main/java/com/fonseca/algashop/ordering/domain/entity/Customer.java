66b26cb40160db11fc41f93c5e78913cc2dd4063
samueljff <samueljosef3@gmail.com>
2025-10-19 14:21:28 -0300

    Implementando e validando regras de negócio com testes unitários

--- src/main/java/com/fonseca/algashop/ordering/domain/entity/Customer.java
+++ src/main/java/com/fonseca/algashop/ordering/domain/entity/Customer.java
@@ -55,7 +55,13 @@ public class Customer {
     }
 
     public void archive() {
-
+        this.setArchived(true);
+        this.setArchivedAt(OffsetDateTime.now());
+        this.setFullName("Anonymous");
+        this.setPhone("000-000-0000");
+        this.setDocument("000-00-0000");
+        this.setEmail(UUID.randomUUID() + "@anonymous.com");
+        this.setBirthDate(null);
     }
 
     public void enablePromotionNotifications() {
