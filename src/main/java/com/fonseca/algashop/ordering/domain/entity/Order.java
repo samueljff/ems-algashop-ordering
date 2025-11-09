4fc55161a8804da626df63ebe49dab496f9acb47
samueljff <samueljosef3@gmail.com>
2025-11-09 16:23:11 -0300

    Invariantes, Propriedades calculadas

--- src/main/java/com/fonseca/algashop/ordering/domain/entity/Order.java
+++ src/main/java/com/fonseca/algashop/ordering/domain/entity/Order.java
@@ -6,6 +6,7 @@ import com.fonseca.algashop.ordering.domain.valueobjet.id.OrderId;
 import com.fonseca.algashop.ordering.domain.valueobjet.id.ProductId;
 import lombok.Builder;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.Collections;
@@ -84,7 +85,7 @@ public class Order {
     }
 
     public void addItem(ProductId productId, ProductName productName,
-                        Money price, Quantity quantity){
+                        Money price, Quantity quantity) {
         OrderItem orderItem = OrderItem.brandNew()
                 .orderId(this.id)
                 .price(price)
@@ -93,11 +94,13 @@ public class Order {
                 .productId(productId)
                 .build();
 
-        if (this.items == null){
+        if (this.items == null) {
             this.items = new HashSet<>();
         }
 
         this.items.add(orderItem);
+
+        this.recalculateTotals();
     }
 
     public OrderId id() {
@@ -158,6 +161,26 @@ public class Order {
 
     public Set<OrderItem> items() {
         return Collections.unmodifiableSet(this.items);
+    }
+
+    private void recalculateTotals() {
+        BigDecimal totalItemsAmount = this.items().stream().map(i -> i.totalAmount().value())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        Integer totalItemsQuantity = this.items().stream().map(i -> i.quantity().value())
+                .reduce(0, Integer::sum);
+
+        BigDecimal shippingCost;
+        if(this.shippingCost() == null) {
+            shippingCost = BigDecimal.ZERO;
+        } else {
+            shippingCost = this.shippingCost.value();
+        }
+
+        BigDecimal totalAmount = totalItemsAmount.add(shippingCost);
+
+        this.setTotalAmount(new Money(totalAmount));
+        this.setTotalItems(new Quantity(totalItemsQuantity));
     }
 
     private void setId(OrderId id) {
