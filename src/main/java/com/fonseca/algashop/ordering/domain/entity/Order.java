8aad096a907d76a97c35d309154900e88ab5298f
samueljff <samueljosef3@gmail.com>
2025-11-15 23:06:52 -0300

    Aprimorando Exceptions com Factory Method

--- src/main/java/com/fonseca/algashop/ordering/domain/entity/Order.java
+++ src/main/java/com/fonseca/algashop/ordering/domain/entity/Order.java
@@ -107,16 +107,7 @@ public class Order {
     }
 
     public void place() {
-        Objects.requireNonNull(this.shipping());
-        Objects.requireNonNull(this.billing());
-        Objects.requireNonNull(this.expectedDeliveryDate());
-        Objects.requireNonNull(this.shippingCost());
-        Objects.requireNonNull(this.paymentMethod());
-        Objects.requireNonNull(this.items());
-
-        if (items().isEmpty()){
-            throw new OrderCannotBePlacedException(this.id);
-        }
+        this.verifyIfCanChangeToPlaced();
         this.changeStatus(OrderStatus.PLACED);
         this.setPlacedAt(OffsetDateTime.now());
     }
@@ -229,6 +220,27 @@ public class Order {
         }
 
         this.setStatus(newStatus);
+    }
+
+    private void verifyIfCanChangeToPlaced() {
+        if (this.shipping() == null) {
+            throw OrderCannotBePlacedException.noShippingInfo(this.id());
+        }
+        if (this.billing() == null) {
+            throw OrderCannotBePlacedException.noBillingInfo(this.id());
+        }
+        if (this.paymentMethod() == null) {
+            throw OrderCannotBePlacedException.noPaymentMethod(this.id());
+        }
+        if (this.shippingCost() == null) {
+            throw OrderCannotBePlacedException.invalidShippingCost(this.id());
+        }
+        if (this.expectedDeliveryDate() == null) {
+            throw OrderCannotBePlacedException.invalidExpectedDeliveryDate(this.id());
+        }
+        if (this.items() == null || this.items().isEmpty()) {
+            throw OrderCannotBePlacedException.noItems(this.id());
+        }
     }
 
     private void recalculateTotals() {
--- src/main/java/com/fonseca/algashop/ordering/domain/exceptions/OrderCannotBePlacedException.java
+++ src/main/java/com/fonseca/algashop/ordering/domain/exceptions/OrderCannotBePlacedException.java
@@ -2,11 +2,35 @@ package com.fonseca.algashop.ordering.domain.exceptions;
 
 import com.fonseca.algashop.ordering.domain.valueobjet.id.OrderId;
 
-import static com.fonseca.algashop.ordering.domain.exceptions.ErrorMessages.ERROR_ORDER_CANNOT_BE_PLACED_HAS_NOT_ITEMS;
+import static com.fonseca.algashop.ordering.domain.exceptions.ErrorMessages.*;
 
 public class OrderCannotBePlacedException extends DomainException {
 
-    public OrderCannotBePlacedException(OrderId id) {
-        super(String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NOT_ITEMS, id));
+    private OrderCannotBePlacedException(String message) {
+        super(message);
+    }
+
+    public static OrderCannotBePlacedException noItems(OrderId id) {
+        return new OrderCannotBePlacedException(String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_ITEMS, id));
+    }
+
+    public static OrderCannotBePlacedException noShippingInfo(OrderId id) {
+        return new OrderCannotBePlacedException(String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_SHIPPING_INFO, id));
+    }
+
+    public static OrderCannotBePlacedException noBillingInfo(OrderId id) {
+        return new OrderCannotBePlacedException(String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_BILLING_INFO, id));
+    }
+
+    public static OrderCannotBePlacedException invalidShippingCost(OrderId id) {
+        return new OrderCannotBePlacedException(String.format(ERROR_ORDER_CANNOT_BE_PLACED_INVALID_SHIPPING_COST, id));
+    }
+
+    public static OrderCannotBePlacedException invalidExpectedDeliveryDate(OrderId id) {
+        return new OrderCannotBePlacedException(String.format(ERROR_ORDER_CANNOT_BE_PLACED_INVALID_DELIVERY_DATE, id));
+    }
+
+    public static OrderCannotBePlacedException noPaymentMethod(OrderId id) {
+        return new OrderCannotBePlacedException(String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_PAYMENT_METHOD, id));
     }
 }
