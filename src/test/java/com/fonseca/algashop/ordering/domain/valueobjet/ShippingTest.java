153d3a23df25bed71b8c124346df3c81ec79b5c8
samueljff <samueljosef3@gmail.com>
2025-11-20 14:03:21 -0300

    Fix: refatorando shipping teste

--- src/test/java/com/fonseca/algashop/ordering/domain/valueobjet/ShippingTest.java
+++ src/test/java/com/fonseca/algashop/ordering/domain/valueobjet/ShippingTest.java
@@ -4,30 +4,17 @@ import com.fonseca.algashop.ordering.domain.entity.OrderTestDataBuilder;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-
-import static com.fonseca.algashop.ordering.domain.entity.OrderTestDataBuilder.anAddressAlt;
-
 class ShippingTest {
 
     @Test
-    void givenValidData_whenCreateShippingInfoWithBuilder_thenShouldSucceed() {
-        // given
+    void givenValidData_whenCreateShippingWithBuilder_thenShouldSucceed() {
+        // given/when
         Document document = new Document("112-33-2321");
         Phone phone = new Phone("111-441-1244");
         FullName fullName = new FullName("John", "Doe");
 
-        Address address =  Address.builder()
-                .street("Bourbon Street")
-                .number("1234")
-                .neighborhood("North Ville")
-                .city("Montfort")
-                .state("South Carolina")
-                .zipCode(new ZipCode("79911"))
-                .complement("apt. 11")
-                .build();
-        // when
-        Shipping shipping = OrderTestDataBuilder.aShipping();
+        Address address = ShippingTestDataBuilder.anAddress();
+        Shipping shipping = ShippingTestDataBuilder.aShipping();
 
         // then
         Assertions.assertThat(shipping.recipient().fullName()).isEqualTo(fullName);
@@ -37,65 +24,44 @@ class ShippingTest {
     }
 
     @Test
-    void givenNullFullName_whenCreateShippingInfo_thenShouldThrowNullPointerException() {
+    void givenNullFullName_whenCreateShipping_thenShouldThrowNullPointerException() {
+        // given/when/then
+        Assertions.assertThatExceptionOfType(NullPointerException.class)
+                .isThrownBy(() -> {
+                    Shipping shipping = ShippingTestDataBuilder.aShippingNullFullName();
+                });
+    }
+
+    @Test
+    void givenNullDocument_whenCreateShipping_thenShouldThrowNullPointerException() {
         // given/then
         Assertions.assertThatExceptionOfType(NullPointerException.class)
                 .isThrownBy(() -> {
-                    Shipping shipping = OrderTestDataBuilder.aShipping().toBuilder()
-                            .recipient(Recipient.builder()
-                                    .fullName(new FullName(null, null))
-                                    .build())
-                            .build();
-
+                    Shipping shipping = ShippingTestDataBuilder.aShippingNullDocument();
                 });
     }
 
     @Test
-    void givenNullDocument_whenCreateShippingInfo_thenShouldThrowNullPointerException() {
+    void givenNullPhone_whenCreateShipping_thenShouldThrowNullPointerException() {
         // given/then
         Assertions.assertThatExceptionOfType(NullPointerException.class)
-                .isThrownBy(() -> {
-                    Shipping shipping = OrderTestDataBuilder.aShipping().toBuilder()
-                            .recipient(Recipient.builder()
-                                    .fullName(new FullName(null, null))
-                                    .build())
-                            .build();
+                .isThrownBy(() -> {Shipping shipping = ShippingTestDataBuilder.aShippingNullPhone();
                 });
     }
 
     @Test
-    void givenNullPhone_whenCreateShippingInfo_thenShouldThrowNullPointerException() {
-        // given/then
-        Assertions.assertThatExceptionOfType(NullPointerException.class)
-                .isThrownBy(() -> {Shipping shipping = OrderTestDataBuilder.aShipping().toBuilder()
-                        .recipient(Recipient.builder()
-                                .fullName(new FullName(null, null))
-                                .build())
-                        .build();
-                });
-    }
-
-    @Test
-    void givenNullAddress_whenCreateShippingInfo_thenShouldThrowNullPointerException() {
+    void givenNullAddress_whenCreateShipping_thenShouldThrowNullPointerException() {
         // when/then
         Assertions.assertThatExceptionOfType(NullPointerException.class)
                 .isThrownBy(() -> {
-                    Address address = anAddressAlt().toBuilder()
-                            .street(null)
-                            .number(null)
-                            .neighborhood(null)
-                            .city("San Francisco")
-                            .state("California")
-                            .zipCode(new ZipCode("08040"))
-                            .build();
+                    Shipping address = ShippingTestDataBuilder.aShippingNullAddress();
                 });
     }
 
     @Test
-    void givenTwoShippingInfosWithSameValues_whenEquals_thenShouldBeEqual() {
-        // given
-        Shipping shipping = OrderTestDataBuilder.aShipping();
-
+    void givenTwoShippingWithSameValues_whenEquals_thenShouldBeEqual() {
+        // given/when
+        Shipping shipping = ShippingTestDataBuilder.aShipping();
         Shipping shipping1 = shipping;
         Shipping shipping2 = shipping;
 
@@ -105,20 +71,10 @@ class ShippingTest {
     }
 
     @Test
-    void givenTwoShippingInfosWithDifferentValues_whenEquals_thenShouldNotBeEqual() {
-        // given
-        Shipping shipping1 = OrderTestDataBuilder.aShipping().toBuilder()
-                .cost(new Money("20.00"))
-                .expectedDate(LocalDate.now().plusWeeks(2))
-                .address(anAddressAlt())
-                .recipient(Recipient.builder()
-                        .fullName(new FullName("Mary", "Jones"))
-                        .document(new Document("552-11-4333"))
-                        .phone(new Phone("54-454-1144"))
-                        .build())
-                .build();
-
-        Shipping shipping2 = OrderTestDataBuilder.aShipping();
+    void givenTwoShippingWithDifferentValues_whenEquals_thenShouldNotBeEqual() {
+        // given/when
+        Shipping shipping1 = ShippingTestDataBuilder.aShipping();
+        Shipping shipping2 = OrderTestDataBuilder.aShippingAlt();
 
         // then
         Assertions.assertThat(shipping1).isNotEqualTo(shipping2);
