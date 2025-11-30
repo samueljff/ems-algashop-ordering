ed67c12dae086603e816fc04cfffcd5af9c7f850
samueljff <samueljosef3@gmail.com>
2025-11-30 14:40:50 -0300

    Isolando execução dos testes de integração

---
+++ src/test/java/com/fonseca/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -0,0 +1,13 @@
+package com.fonseca.algashop.ordering.infrastructure.persistence.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPersistenceEntityRepositoryIT {
+
+    @Test
+    public void shouldRun(){
+
+    }
+}
