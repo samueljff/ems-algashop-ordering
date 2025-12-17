a1fe3f2bbf483e9d226424b9c4a66fa1c1a2452a
samueljff <samueljosef3@gmail.com>
2025-12-16 22:41:17 -0300

    Persistindo Entities em cascata

--- src/test/java/com/fonseca/algashop/ordering/infrastructure/persistence/entity/OrderPersistenceEntityTestDataBuilder.java
+++ src/test/java/com/fonseca/algashop/ordering/infrastructure/persistence/entity/OrderPersistenceEntityTestDataBuilder.java
@@ -4,6 +4,8 @@ import com.fonseca.algashop.ordering.domain.model.utility.IdGenerator;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.fonseca.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity.*;
 
@@ -16,10 +18,31 @@ public class OrderPersistenceEntityTestDataBuilder {
         return OrderPersistenceEntity.builder()
                 .id(IdGenerator.generateTSID().toLong())
                 .customerId(IdGenerator.generateTimeBasedUUID())
-                .totalItems(2)
-                .totalAmount(new BigDecimal(1000))
+                .totalItems(3)
+                .totalAmount(new BigDecimal(1250))
                 .status("DRAFT")
                 .paymentMethod("CREDIT_CARD")
-                .placedAt(OffsetDateTime.now());
+                .placedAt(OffsetDateTime.now())
+                .items(Set.of(existingItem().build(), existingItemAlt().build()));
+    }
+
+    public static OrderItemPersistenceEntity.OrderItemPersistenceEntityBuilder existingItem() {
+        return OrderItemPersistenceEntity.builder()
+                .id(IdGenerator.generateTSID().toLong())
+                .price(new BigDecimal(500))
+                .quantity(2)
+                .totalAmount(new BigDecimal(1000))
+                .productName("Notebook")
+                .productId(IdGenerator.generateTimeBasedUUID());
+    }
+
+    public static OrderItemPersistenceEntity.OrderItemPersistenceEntityBuilder existingItemAlt() {
+        return OrderItemPersistenceEntity.builder()
+                .id(IdGenerator.generateTSID().toLong())
+                .price(new BigDecimal(250))
+                .quantity(1)
+                .totalAmount(new BigDecimal(250))
+                .productName("Mouse Pad")
+                .productId(IdGenerator.generateTimeBasedUUID());
     }
 }
