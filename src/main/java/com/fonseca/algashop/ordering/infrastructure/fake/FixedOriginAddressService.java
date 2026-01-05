98ff71b9c63f7e4d137e301f490b2a06d2d1731c
samueljff <samueljosef3@gmail.com>
2026-01-05 20:22:14 -0300

    Domain Service para buscar endereço AlgaShop

---
+++ src/main/java/com/fonseca/algashop/ordering/infrastructure/fake/FixedOriginAddressService.java
@@ -0,0 +1,20 @@
+package com.fonseca.algashop.ordering.infrastructure.fake;
+
+import com.fonseca.algashop.ordering.domain.model.service.OriginAddressService;
+import com.fonseca.algashop.ordering.domain.model.valueObject.Address;
+import com.fonseca.algashop.ordering.domain.model.valueObject.ZipCode;
+
+public class FixedOriginAddressService implements OriginAddressService {
+
+    @Override
+    public Address originAddress() {
+        return Address.builder()
+                .street("Bourbon Street")
+                .number("1134")
+                .neighborhood("North Ville")
+                .city("York")
+                .state("South California")
+                .zipCode(new ZipCode("12345"))
+                .build();
+    }
+}
