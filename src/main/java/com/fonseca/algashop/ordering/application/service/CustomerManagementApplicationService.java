0fe902163cc044adb6dfb3e3002587cad0fa63fc
samueljff <samueljosef3@gmail.com>
2026-01-14 19:35:15 -0300

    Consulta de dados no Application Service

--- src/main/java/com/fonseca/algashop/ordering/application/service/CustomerManagementApplicationService.java
+++ src/main/java/com/fonseca/algashop/ordering/application/service/CustomerManagementApplicationService.java
@@ -2,11 +2,9 @@ package com.fonseca.algashop.ordering.application.service;
 
 import com.fonseca.algashop.ordering.application.model.AddressData;
 import com.fonseca.algashop.ordering.application.model.CustomerInput;
+import com.fonseca.algashop.ordering.application.model.CustomerOutput;
 import com.fonseca.algashop.ordering.domain.model.commons.*;
-import com.fonseca.algashop.ordering.domain.model.customer.BirthDate;
-import com.fonseca.algashop.ordering.domain.model.customer.Customer;
-import com.fonseca.algashop.ordering.domain.model.customer.CustomerRegistrationService;
-import com.fonseca.algashop.ordering.domain.model.customer.Customers;
+import com.fonseca.algashop.ordering.domain.model.customer.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -46,5 +44,35 @@ public class CustomerManagementApplicationService {
         customers.add(customer);
 
         return customer.id().value();
+    }
+
+    @Transactional(readOnly = true)
+    public CustomerOutput findById(UUID customerId){
+        Objects.requireNonNull(customerId);
+        Customer customer = customers.ofId(new CustomerId(customerId)).orElseThrow(() -> new CustomerNotFoundException());
+
+        return CustomerOutput.builder()
+                .id(customer.id().value())
+                .firstName(customer.fullName().firstName())
+                .lastName(customer.fullName().lastName())
+                .email(customer.email().value())
+                .document(customer.document().value())
+                .phone(customer.phone().value())
+                .promotionNotificationsAllowed(customer.isPromotionNotificationsAllowed())
+                .loyaltyPoints(customer.loyaltyPoints().value())
+                .registeredAt(customer.registeredAt())
+                .archived(customer.isArchived())
+                .archivedAt(customer.archivedAt() != null ? customer.archivedAt() : null)
+                .birthDate(customer.birthDate() != null ? customer.birthDate().value() : null)
+                .address(AddressData.builder()
+                        .street(customer.address().street())
+                        .number(customer.address().number())
+                        .complement(customer.address().complement())
+                        .neighborhood(customer.address().neighborhood())
+                        .city(customer.address().city())
+                        .state(customer.address().state())
+                        .zipCode(customer.address().zipCode().value())
+                        .build())
+                .build();
     }
 }
--- src/test/java/com/fonseca/algashop/ordering/application/service/CustomerManagementApplicationServiceIT.java
+++ src/test/java/com/fonseca/algashop/ordering/application/service/CustomerManagementApplicationServiceIT.java
@@ -2,6 +2,7 @@ package com.fonseca.algashop.ordering.application.service;
 
 import com.fonseca.algashop.ordering.application.model.AddressData;
 import com.fonseca.algashop.ordering.application.model.CustomerInput;
+import com.fonseca.algashop.ordering.application.model.CustomerOutput;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,5 +39,14 @@ class CustomerManagementApplicationServiceIT {
 
         UUID customerId = customerManagementApplicationService.create(input);
         Assertions.assertThat(customerId).isNotNull();
+
+        CustomerOutput customerOutput = customerManagementApplicationService.findById(customerId);
+
+        Assertions.assertThat(customerOutput.getId()).isEqualTo(customerId);
+        Assertions.assertThat(customerOutput.getFirstName()).isEqualTo("John");
+        Assertions.assertThat(customerOutput.getLastName()).isEqualTo("Doe");
+        Assertions.assertThat(customerOutput.getEmail()).isEqualTo("johndoe@email.com");
+        Assertions.assertThat(customerOutput.getBirthDate()).isEqualTo(LocalDate.of(1991, 7,5));
+        Assertions.assertThat(customerOutput.getRegisteredAt()).isNotNull();
     }
 }
