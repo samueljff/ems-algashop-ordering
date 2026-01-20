01549760c340b7f78716d337c11a81f2913f50e5
samueljff <samueljosef3@gmail.com>
2026-01-19 21:26:53 -0300

    Application Service para adição de Loyalty Points

---
+++ src/main/java/com/fonseca/algashop/ordering/domain/model/order/OrderNotFoundException.java
@@ -0,0 +1,6 @@
+package com.fonseca.algashop.ordering.domain.model.order;
+
+import com.fonseca.algashop.ordering.domain.model.DomainException;
+
+public class OrderNotFoundException extends DomainException {
+}
