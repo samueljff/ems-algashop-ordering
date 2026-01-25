f5d34a838f604cfdd916cde5351a297d5d8381cb
samueljff <samueljosef3@gmail.com>
2026-01-25 17:59:30 -0300

    Implementando infraestrutura para publicação de eventos

--- src/main/java/com/fonseca/algashop/ordering/infrastructure/persistence/customer/CustomerPersistenceEntity.java
+++ src/main/java/com/fonseca/algashop/ordering/infrastructure/persistence/customer/CustomerPersistenceEntity.java
@@ -6,10 +6,12 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.util.Collection;
 import java.util.UUID;
 
 @Entity
@@ -17,12 +19,12 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString(of = "id")
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @EntityListeners(AuditingEntityListener.class)
-public class CustomerPersistenceEntity {
+public class  CustomerPersistenceEntity extends AbstractAggregateRoot<CustomerPersistenceEntity> {
 
     @Id
     @EqualsAndHashCode.Include
@@ -61,4 +63,16 @@ public class CustomerPersistenceEntity {
 
     @Version
     private Long version;
+
+    public Collection<Object> getEvents(){
+        return super.domainEvents();
+    }
+
+    public void addEvents(Collection<Object> events){
+        if (events != null){
+            for (Object event : events){
+                this.registerEvent(event);
+            }
+        }
+    }
 }
