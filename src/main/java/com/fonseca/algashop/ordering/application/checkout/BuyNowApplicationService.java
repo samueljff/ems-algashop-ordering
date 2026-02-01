fcb17026398ed9467b185d0a04f21a2a504cce1e
samueljff <samueljosef3@gmail.com>
2026-02-01 15:17:22 -0300

    Implementando regra para frete grátis

--- src/main/java/com/fonseca/algashop/ordering/application/checkout/BuyNowApplicationService.java
+++ src/main/java/com/fonseca/algashop/ordering/application/checkout/BuyNowApplicationService.java
@@ -2,7 +2,10 @@ package com.fonseca.algashop.ordering.application.checkout;
 
 import com.fonseca.algashop.ordering.domain.model.commons.Quantity;
 import com.fonseca.algashop.ordering.domain.model.commons.ZipCode;
+import com.fonseca.algashop.ordering.domain.model.customer.Customer;
 import com.fonseca.algashop.ordering.domain.model.customer.CustomerId;
+import com.fonseca.algashop.ordering.domain.model.customer.CustomerNotFoundException;
+import com.fonseca.algashop.ordering.domain.model.customer.Customers;
 import com.fonseca.algashop.ordering.domain.model.order.*;
 import com.fonseca.algashop.ordering.domain.model.order.shipping.OriginAddressService;
 import com.fonseca.algashop.ordering.domain.model.order.shipping.ShippingCostService;
@@ -27,6 +30,7 @@ public class BuyNowApplicationService {
     private final OriginAddressService originAddressService;
 
     private final Orders orders;
+    private final Customers customers;
 
     private final ShippingInputDisassembler shippingInputDisassembler;
     private final BillingInputDisassembler billingInputDisassembler;
@@ -39,6 +43,8 @@ public class BuyNowApplicationService {
         CustomerId customerId = new CustomerId(input.getCustomerId());
         Quantity quantity = new Quantity(input.getQuantity());
 
+        Customer customer = customers.ofId(customerId).orElseThrow(() -> new CustomerNotFoundException());
+
         Product product = findProduct(new ProductId(input.getProductId()));
 
         var shippingCalculationResult = calculateShippingCost(input.getShipping());
@@ -49,7 +55,7 @@ public class BuyNowApplicationService {
         Billing billing = billingInputDisassembler.toDomainModel(input.getBilling());
 
         Order order = buyNowService.buyNow(
-                product, customerId, billing, shipping, quantity, paymentMethod
+                product, customer, billing, shipping, quantity, paymentMethod
         );
 
         orders.add(order);
--- src/main/java/com/fonseca/algashop/ordering/domain/model/order/BuyNowService.java
+++ src/main/java/com/fonseca/algashop/ordering/domain/model/order/BuyNowService.java
@@ -1,15 +1,24 @@
 package com.fonseca.algashop.ordering.domain.model.order;
 
 import com.fonseca.algashop.ordering.domain.model.DomainService;
+import com.fonseca.algashop.ordering.domain.model.commons.Money;
+import com.fonseca.algashop.ordering.domain.model.customer.Customer;
+import com.fonseca.algashop.ordering.domain.model.customer.LoyaltyPoints;
 import com.fonseca.algashop.ordering.domain.model.product.Product;
 import com.fonseca.algashop.ordering.domain.model.commons.Quantity;
 import com.fonseca.algashop.ordering.domain.model.customer.CustomerId;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Year;
 
 @DomainService
+@RequiredArgsConstructor
 public class BuyNowService {
 
+    private Orders orders;
+
     public Order buyNow(Product product,
-                        CustomerId customerId,
+                        Customer customer,
                         Billing billing,
                         Shipping shipping,
                         Quantity quantity,
@@ -17,14 +26,25 @@ public class BuyNowService {
     ) {
         product.checkOutOfStock();
 
-        Order order = Order.draft(customerId);
+        Order order = Order.draft(customer.id());
         order.changeBilling(billing);
-        order.changeShipping(shipping);
         order.changePaymentMethod(paymentMethod);
-
         order.addItem(product, quantity);
+
+        if (haveFreeShipping(customer)){
+            Shipping freeShipping = shipping.toBuilder().cost(Money.ZERO).build();
+            order.changeShipping(freeShipping);
+        } else {
+            order.changeShipping(shipping);
+        }
         order.place();
 
         return order;
+    }
+
+    private boolean haveFreeShipping(Customer customer) {
+        return customer.loyaltyPoints().compareTo(new LoyaltyPoints(100)) >=0
+                && orders.salesQuantityByCustomerInYear(customer.id(), Year.now()) >= 2
+                || customer.loyaltyPoints().compareTo(new LoyaltyPoints(2000)) >= 0;
     }
 }
