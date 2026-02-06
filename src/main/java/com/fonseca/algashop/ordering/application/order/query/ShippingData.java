077858b01261bf3e0b1edb3b99098599e86cc99c
samueljff <samueljosef3@gmail.com>
2026-02-05 22:58:59 -0300

    Modelo de leitura detalhado para Order

---
+++ src/main/java/com/fonseca/algashop/ordering/application/order/query/ShippingData.java
@@ -0,0 +1,22 @@
+package com.fonseca.algashop.ordering.application.order.query;
+
+import com.fonseca.algashop.ordering.application.checkout.RecipientData;
+import com.fonseca.algashop.ordering.application.commons.AddressData;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ShippingData {
+    private BigDecimal cost;
+    private LocalDate expectedDate;
+    private RecipientData recipient;
+    private AddressData address;
+}
