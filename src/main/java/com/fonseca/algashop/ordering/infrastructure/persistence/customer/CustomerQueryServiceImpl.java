f95e04cc5b8c15851e30c234cd743f1d1bc3d436
samueljff <samueljosef3@gmail.com>
2026-02-05 22:30:37 -0300

    Implementação simplificada com Entity Manager

--- src/main/java/com/fonseca/algashop/ordering/infrastructure/persistence/customer/CustomerQueryServiceImpl.java
+++ src/main/java/com/fonseca/algashop/ordering/infrastructure/persistence/customer/CustomerQueryServiceImpl.java
@@ -2,23 +2,58 @@ package com.fonseca.algashop.ordering.infrastructure.persistence.customer;
 
 import com.fonseca.algashop.ordering.application.customer.query.CustomerOutput;
 import com.fonseca.algashop.ordering.application.customer.query.CustomerQueryService;
-import com.fonseca.algashop.ordering.application.utility.Mapper;
 import com.fonseca.algashop.ordering.domain.model.customer.CustomerNotFoundException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Objects;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CustomerQueryServiceImpl implements CustomerQueryService {
 
-    private final CustomerPersistenceEntityRepository repository;
+    private final EntityManager entityManager;
+
+    private static final String findByIdAsOutputJPQL = """
+            SELECT new com.fonseca.algashop.ordering.application.customer.query.CustomerOutput(
+                c.id,
+                c.firstName,
+                c.lastName,
+                c.email,
+                c.document,
+                c.phone,
+                c.birthDate,
+                c.loyaltyPoints,
+                c.registeredAt,
+                c.archivedAt,
+                c.promotionNotificationsAllowed,
+                c.archived,
+                new com.fonseca.algashop.ordering.application.commons.AddressData(
+                    c.address.street,
+                    c.address.number,
+                    c.address.complement,
+                    c.address.neighborhood,
+                    c.address.city,
+                    c.address.state,
+                    c.address.zipCode
+                )
+            )
+            FROM CustomerPersistenceEntity c
+            WHERE c.id = :id""";
 
     @Override
     public CustomerOutput findById(UUID customerId) {
-        Objects.requireNonNull(customerId);
-        return repository.findByIdAsOutput(customerId).orElseThrow(() -> new CustomerNotFoundException());
+        try {
+            TypedQuery<CustomerOutput> query = entityManager.createQuery(findByIdAsOutputJPQL, CustomerOutput.class);
+            query.setParameter("id", customerId);
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            throw new CustomerNotFoundException();
+        }
     }
 }
