fd20ebbac8bc42f25c024214fa7f7800eb8169e5
samueljff <samueljosef3@gmail.com>
2026-02-15 13:13:45 -0300

    Consulta de carrinhos

---
+++ src/main/java/com/fonseca/algashop/ordering/infrastructure/persistence/shoppingcart/ShoppingCartQueryServiceIml.java
@@ -0,0 +1,34 @@
+package com.fonseca.algashop.ordering.infrastructure.persistence.shoppingcart;
+
+import com.fonseca.algashop.ordering.application.shoppingcart.query.ShoppingCartOutput;
+import com.fonseca.algashop.ordering.application.shoppingcart.query.ShoppingCartQueryService;
+import com.fonseca.algashop.ordering.application.utility.Mapper;
+import com.fonseca.algashop.ordering.domain.model.shoppingcart.ShoppingCartNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ShoppingCartQueryServiceIml implements ShoppingCartQueryService {
+
+    private final ShoppingCartPersistenceEntityRepository persistenceRepository;
+    private final Mapper mapper;
+
+    @Override
+    public ShoppingCartOutput findById(UUID shoppingCartId) {
+        return persistenceRepository.findById(shoppingCartId)
+                .map(sc -> mapper.convert(sc, ShoppingCartOutput.class))
+                .orElseThrow(() -> new ShoppingCartNotFoundException());
+    }
+
+    @Override
+    public ShoppingCartOutput findByCustomerId(UUID customerId) {
+        return persistenceRepository.findByCustomer_Id(customerId)
+                .map(sc -> mapper.convert(sc, ShoppingCartOutput.class))
+                .orElseThrow(() -> new ShoppingCartNotFoundException());
+    }
+}
