79a1368f14f218d41468bd98a9388d57eb82ffb7
samueljff <samueljosef3@gmail.com>
2026-04-04 22:14:05 -0300

    Testando endpoint de criação de cliente

---
+++ src/main/java/com/fonseca/algashop/ordering/presetation/CustomerController.java
@@ -0,0 +1,36 @@
+package com.fonseca.algashop.ordering.presetation;
+
+import com.fonseca.algashop.ordering.application.commons.AddressData;
+import com.fonseca.algashop.ordering.application.customer.management.CustomerInput;
+import com.fonseca.algashop.ordering.application.customer.query.CustomerOutput;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomerController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerOutput create(@RequestBody CustomerInput input){
+
+        AddressData address = input.getAddress();
+        return CustomerOutput.builder()
+                .id(UUID.randomUUID())
+                .phone(input.getPhone())
+                .email(input.getEmail())
+                .firstName(input.getFirstName())
+                .lastName(input.getLastName())
+                .birthDate(input.getBirthDate())
+                .document(input.getDocument())
+                .promotionNotificationsAllowed(input.getPromotionNotificationsAllowed())
+                .registeredAt(OffsetDateTime.now())
+                .archived(false)
+                .loyaltyPoints(0)
+                .address(address)
+                .build();
+    }
+}
