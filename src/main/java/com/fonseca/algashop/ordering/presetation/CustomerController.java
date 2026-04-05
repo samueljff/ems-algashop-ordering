0135c4129e6935cb604b27db2eddaf25098785e3
samueljff <samueljosef3@gmail.com>
2026-04-04 22:56:39 -0300

    Usando Mock no teste de contrato

--- src/main/java/com/fonseca/algashop/ordering/presetation/CustomerController.java
+++ src/main/java/com/fonseca/algashop/ordering/presetation/CustomerController.java
@@ -1,36 +1,27 @@
 package com.fonseca.algashop.ordering.presetation;
 
-import com.fonseca.algashop.ordering.application.commons.AddressData;
 import com.fonseca.algashop.ordering.application.customer.management.CustomerInput;
+import com.fonseca.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
 import com.fonseca.algashop.ordering.application.customer.query.CustomerOutput;
+import com.fonseca.algashop.ordering.application.customer.query.CustomerQueryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerManagementApplicationService customerManagementApplicationService;
+    private final CustomerQueryService customerQueryService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CustomerOutput create(@RequestBody CustomerInput input){
-
-        AddressData address = input.getAddress();
-        return CustomerOutput.builder()
-                .id(UUID.randomUUID())
-                .phone(input.getPhone())
-                .email(input.getEmail())
-                .firstName(input.getFirstName())
-                .lastName(input.getLastName())
-                .birthDate(input.getBirthDate())
-                .document(input.getDocument())
-                .promotionNotificationsAllowed(input.getPromotionNotificationsAllowed())
-                .registeredAt(OffsetDateTime.now())
-                .archived(false)
-                .loyaltyPoints(0)
-                .address(address)
-                .build();
+        UUID customerId = customerManagementApplicationService.create(input);
+        return customerQueryService.findById(customerId);
     }
 }
