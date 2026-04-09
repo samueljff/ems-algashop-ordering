4785bc0aa476ec116172718fe95c13f5ddd65ed8
samueljff <samueljosef3@gmail.com>
2026-04-08 22:54:33 -0300

    Testes de contrato com paginação

--- src/main/java/com/fonseca/algashop/ordering/presetation/CustomerController.java
+++ src/main/java/com/fonseca/algashop/ordering/presetation/CustomerController.java
@@ -2,8 +2,10 @@ package com.fonseca.algashop.ordering.presetation;
 
 import com.fonseca.algashop.ordering.application.customer.management.CustomerInput;
 import com.fonseca.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
+import com.fonseca.algashop.ordering.application.customer.query.CustomerFilter;
 import com.fonseca.algashop.ordering.application.customer.query.CustomerOutput;
 import com.fonseca.algashop.ordering.application.customer.query.CustomerQueryService;
+import com.fonseca.algashop.ordering.application.customer.query.CustomerSummaryOutput;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,5 +26,10 @@ public class CustomerController {
     public CustomerOutput create(@RequestBody @Valid CustomerInput input){
         UUID customerId = customerManagementApplicationService.create(input);
         return customerQueryService.findById(customerId);
+    }
+
+    @GetMapping
+    public PageModel<CustomerSummaryOutput> findAll(CustomerFilter customerFilter) {
+        return PageModel.of(customerQueryService.filter(customerFilter));
     }
 }
