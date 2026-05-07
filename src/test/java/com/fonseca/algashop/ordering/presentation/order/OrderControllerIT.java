15db3c0c4e89b409050888c6692e41274c7b9f10
samueljff <samueljosef3@gmail.com>
2026-05-06 22:35:42 -0300

    Testes usando Stub gerado pelo Spring Cloud Contract

--- src/test/java/com/fonseca/algashop/ordering/presentation/order/OrderControllerIT.java
+++ src/test/java/com/fonseca/algashop/ordering/presentation/order/OrderControllerIT.java
@@ -20,6 +20,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -29,6 +31,7 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 import static io.restassured.config.JsonConfig.jsonConfig;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL, ids = "com.fonseca.algashop:product-catalog:0.0.1-SNAPSHOT:8781")
 public class OrderControllerIT {
 
     @LocalServerPort
