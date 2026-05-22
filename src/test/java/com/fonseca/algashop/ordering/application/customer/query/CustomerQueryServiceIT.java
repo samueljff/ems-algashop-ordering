0ae0de0c14a6bc63eaa8c65a462d42f524eab27d
samueljff <samueljosef3@gmail.com>
2026-05-21 22:50:43 -0300

    Configurando um banco de dados real para testes de integração

--- src/test/java/com/fonseca/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
+++ src/test/java/com/fonseca/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.OffsetDateTime;
