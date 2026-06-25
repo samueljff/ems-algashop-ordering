a11b77aaa11ff7bfe8cdb55e3aa51930d3ceef05
samueljff <samueljosef3@gmail.com>
2026-06-24 23:40:43 -0300

    Adicionando Testcontainer aos testes de Presentation

--- src/test/java/com/fonseca/algashop/ordering/presentation/shoppingcart/ShoppingCartControllerIT.java
+++ src/test/java/com/fonseca/algashop/ordering/presentation/shoppingcart/ShoppingCartControllerIT.java
@@ -3,36 +3,20 @@ package com.fonseca.algashop.ordering.presentation.shoppingcart;
 import com.fonseca.algashop.ordering.infrastructure.persistence.customer.CustomerPersistenceEntityRepository;
 import com.fonseca.algashop.ordering.infrastructure.persistence.shoppingcart.ShoppingCartPersistenceEntity;
 import com.fonseca.algashop.ordering.infrastructure.persistence.shoppingcart.ShoppingCartPersistenceEntityRepository;
+import com.fonseca.algashop.ordering.presentation.AbstractPresentationIT;
 import com.fonseca.algashop.ordering.utils.AlgaShopResourceUtils;
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import io.restassured.RestAssured;
-import io.restassured.path.json.config.JsonPathConfig;
 import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
 import java.util.UUID;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static io.restassured.config.JsonConfig.jsonConfig;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "classpath:db/testdata/afterMigrate.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
-@Sql(scripts = "classpath:db/clean/afterMigrate.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS)
-public class ShoppingCartControllerIT {
-
-    @LocalServerPort
-    private int port;
+public class ShoppingCartControllerIT extends AbstractPresentationIT {
 
     @Autowired
     private CustomerPersistenceEntityRepository customerRepository;
@@ -43,30 +27,19 @@ public class ShoppingCartControllerIT {
     private static final UUID validCustomerId = UUID.fromString("3a4b5c6d-7e8f-9a0b-1c2d-3e4f5a6b7c8d");
     private static final UUID validShoppingCartId = UUID.fromString("4f31582a-66e6-4601-a9d3-ff608c2d4461");
 
-    private WireMockServer wireMockProductCatalog;
-
     @BeforeEach
     public void setup() {
-
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-        RestAssured.port = port;
-
-        RestAssured.config().jsonConfig(
-            jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.BIG_DECIMAL)
-        );
-
-        wireMockProductCatalog = new WireMockServer(options()
-            .port(8781)
-            .usingFilesUnderDirectory("src/test/resources/wiremock/product-catalog")
-            .extensions(new ResponseTemplateTransformer(true))
-        );
-
-        wireMockProductCatalog.start();
+        beforeEach();
     }
 
-    @AfterEach
-    public void after() {
-        wireMockProductCatalog.stop();
+    @BeforeAll
+    public static void setupBeforeAll() {
+        initWireMock();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        stopMock();
     }
 
     @Test
