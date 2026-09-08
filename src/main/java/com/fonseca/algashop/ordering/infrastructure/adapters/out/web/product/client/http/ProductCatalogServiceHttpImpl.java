2b00404e7db1f195ffe9290e6c124f55b895bd7a
samueljff <samueljosef3@gmail.com>
2026-09-07 23:08:20 -0300

    Utilizando Bulkhead Pattern com @ConcurrencyLimit

--- src/main/java/com/fonseca/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ProductCatalogServiceHttpImpl.java
+++ src/main/java/com/fonseca/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ProductCatalogServiceHttpImpl.java
@@ -9,6 +9,7 @@ import com.fonseca.algashop.ordering.infrastructure.adapters.in.web.exceptionhan
 import com.fonseca.algashop.ordering.infrastructure.adapters.in.web.exceptionhandler.GatewayTimeoutException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.resilience.annotation.ConcurrencyLimit;
 import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -16,6 +17,7 @@ import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientException;
 
 import java.net.SocketTimeoutException;
+import java.time.Duration;
 import java.util.Optional;
 
 @Component
@@ -25,6 +27,7 @@ public class ProductCatalogServiceHttpImpl implements ProductCatalogService {
 
     private final ProductCatalogAPIClient productCatalogAPIClient;
 
+    @ConcurrencyLimit(10)
     @Retryable (
         maxRetries = 3,
         delayString = "3s",
@@ -33,6 +36,14 @@ public class ProductCatalogServiceHttpImpl implements ProductCatalogService {
     )
     @Override
     public Optional<Product> ofId(ProductId productId) {
+        log.info("Trying to load product {}", productId);
+
+        try {
+            Thread.sleep(Duration.ofSeconds(3));
+        } catch (Exception e){
+
+        }
+
         ProductResponse productResponse;
         log.info("Loading product {}", productId);
         try {
