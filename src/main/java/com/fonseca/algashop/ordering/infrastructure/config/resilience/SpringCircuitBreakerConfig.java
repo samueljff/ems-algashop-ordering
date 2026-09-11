7abd220f4af957741bbb5f17184c8bd05664e297
samueljff <samueljosef3@gmail.com>
2026-09-10 23:40:54 -0300

    Visualizando estado do Circuit Breaker

--- src/main/java/com/fonseca/algashop/ordering/infrastructure/config/resilience/SpringCircuitBreakerConfig.java
+++ src/main/java/com/fonseca/algashop/ordering/infrastructure/config/resilience/SpringCircuitBreakerConfig.java
@@ -25,8 +25,8 @@ public class SpringCircuitBreakerConfig {
         return factory -> {
             factory.configure(builder -> builder
                 .retryPolicy(retryPolicy)
-                .openTimeout(Duration.ofSeconds(10))
-                .resetTimeout(Duration.ofSeconds(25))
+                .openTimeout(Duration.ofSeconds(15))
+                .resetTimeout(Duration.ofSeconds(30))
                 .build(), "productCatalogCB"
             );
         };
